8053aa61275d2fefb0f1a101a2a62dea0b7a3c1c
zakaria <zakariabanouri52@gmail.com>
2021-10-01 11:49:36 +0200

    Test git fil2

--- src/lektion11/opgave1/Test.java
+++ src/lektion11/opgave1/Test.java
@@ -3,5 +3,7 @@ package lektion11.opgave1;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test git");
+
+        System.out.println("test git2");
     }
 }
